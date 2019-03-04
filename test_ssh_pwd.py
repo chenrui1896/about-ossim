e10cd3e123ce3ad76ce6a7a2d445eab9a501d136
mLLLs <33108041+chenrui1896@users.noreply.github.com>
2019-03-04 15:42:38 +0800

    Create test_ssh_pwd.py

---
+++ test_ssh_pwd.py
@@ -0,0 +1,30 @@
+import paramiko
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+with open('/test/test') as f:
+	lines = f.readlines()
+
+rightPWd = open('right_pwd.txt', 'w+')
+wrongPWD = open('wrong_pwd.txt', 'w+')
+for line in lines:
+	ip, port, pwd = line.split(' ')
+	try:
+		ssh.connect(hostname=ip, port=22, username='root', password=pwd)
+		#rightPWd.write(line)
+		print line
+	except Exception as e:
+		#print e
+		if 'No route ' in e:
+			wrongPWD.write('Dead '+line)
+		elif 'failed' in e:
+			wrongPWD.write('Wrong '+line)
+		elif 'refused' in e:
+			rightPWd.write('Unknown '+line)
+		else:
+			rightPWd.write(line)
+		continue
+
+rightPWd.close()
+wrongPWD.close()
